9caeeff9008d42778ebc06bae0d7165f715b5db7
beplop <56293559+beplop@users.noreply.github.com>
2019-11-17 20:46:47 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,47 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Авторизация</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<?php
-require('db.php');
-session_start();
-// If form submitted, insert values into the database.
-if (isset($_POST['username'])){
-        // removes backslashes
- $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
- $username = mysqli_real_escape_string($con,$username);
- $password = stripslashes($_REQUEST['password']);
- $password = mysqli_real_escape_string($con,$password);
- //Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
-and password='".md5($password)."'";
- $result = mysqli_query($con,$query) or die(mysql_error());
- $rows = mysqli_num_rows($result);
-        if($rows==1){
-     $_SESSION['username'] = $username;
-            // Redirect user to index.php
-     header("Location: index.php");
-         }else{
- echo "<div class='form'>
-<h3>Username/password is incorrect.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
- }
-    }else{
-?>
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
-</div>
-<?php } ?>
-</body>
+     <head>
+         <meta charset="utf-8">
+         <title>Авторизация</title>
+         <link rel="stylesheet" href="css/style.css" />
+     </head>
+     <body>
+          <?php
+                require('db.php');
+                session_start();
+                if (isset($_POST['username'])){
+                 $username = stripslashes($_REQUEST['username']);
+                 $username = mysqli_real_escape_string($con,$username);
+                 $password = stripslashes($_REQUEST['password']);
+                 $password = mysqli_real_escape_string($con,$password);
+                        $query = "SELECT * FROM `users` WHERE username='$username'
+                and password='".md5($password)."'";
+                 $result = mysqli_query($con,$query) or die(mysql_error());
+                 $rows = mysqli_num_rows($result);
+                        if($rows==1){
+                     $_SESSION['username'] = $username;
+                     header("Location: index.php");
+                         }else{
+                 echo "<div class='form'>
+                <h3>Имя пользователя или пароль некорректны.</h3>
+                <br/>Нажмите сюда для <a href='login.php'>Авторизации</a></div>";
+                 }
+                    }else{
+          ?>
+          <div class="form">
+                 <h1>Вход</h1>
+                 <form action="" method="post" name="login">
+                      <input type="text" name="username" placeholder="Никнейм"required />     
+                      <input type="password" name="password" placeholder="Пароль" required />       
+                      <input name="submit" type="submit" value="Login" />       
+                 </form>
+                 <p>Еще не зарегистрированы? <a href='registration.php'>Зарегистрируйтесь здесь</a></p>
+          </div>
+          <?php } ?>
+     </body>
 </html>
