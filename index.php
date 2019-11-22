50e937143e35e73c2711dd352f70e789f14872f5
beplop <56293559+beplop@users.noreply.github.com>
2019-11-23 01:21:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,20 @@ include("auth.php");
 <html>
      <head>
          <meta charset="utf-8">
-         <title>Welcome Home</title>
+         <title>Math4YOU</title>
          <link rel="stylesheet" href="css/style.css" />
      </head>
      <body>
-         <div class="form">
-             <p>Welcome <?php echo $_SESSION['username']; ?>!</p>
-             <p>This is secure area.</p>
-             <p><a href="dashboard.php">Dashboard</a></p>
-             <a href="logout.php">Logout</a>
-         </div>
+            <div class="head">
+                  <p id="math" align="center">Математический форум</p>
+            </div>
+            <div class="user">
+             <p>Пользователь: <?php echo $_SESSION['username']; ?></p>
+             <p><a href="dashboard.php">Ваш профиль</a></p>
+             <a href="logout.php">Выйти</a>
+            </div>
+            <div class="links">ссылки</div>
+            <div class="big">основная инфа</div>
+            <div class="float">подвал</div>
      </body>
 </html>
