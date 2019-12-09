8eb0cda551902a755c1b327a1c337ab0d96fe730
beplop <56293559+beplop@users.noreply.github.com>
2019-12-09 20:02:17 +0300

    Update statya.php

--- statya.php
+++ statya.php
@@ -13,7 +13,7 @@ include("auth.php");
             <div class="head">
                 <p id="math" align="center">Математический форум</p>
             </div>
-            <div id="bad_users" style="margin-left: 1236px">
+            <div id="bad_users" style="margin-left: 1225px">
                 <p>Пользователь: <?php echo $_SESSION['username']; ?></p>
                 <p><a href="dashboard.php">Ваш профиль</a></p>
                 <a href="logout.php">Выйти</a>
@@ -24,10 +24,60 @@ include("auth.php");
             <a href="index.php">&#8226 Главная</a> <br>
             <a href="formulas.php">&#8226 Формулы</a>
             <a href="game.php">&#8226 Игра</a>
+            <br>
+            <a href="statya.php">&#8226 Статья</a>
         </div>
          <div id="bad_big">
                 <div align="center">
                     <h1 class="headline">Статья</h1>
+                </div>
+                <div>
+                    <?php
+                        $con = mysqli_connect("localhost","root","ralibu94","content");
+                        $sql = mysqli_query($con, 'SELECT `text` FROM `content`');
+                        $result = mysqli_fetch_assoc($sql);
+            if ($_SESSION['auth'] == true and $_SESSION['status'] == 10) {       //для админа возможности
+    if (isset($_POST["text"])) {
+      if (isset($_GET['red_id'])) {
+          $sql = mysqli_query($con, "UPDATE `content` SET `text` = '{$_POST['text']}' WHERE `ID`={$_GET['red_id']}");
+      } 
+      if ($sql) {
+        echo '<p>Успешно!</p>';
+      } else {
+        echo '<p>Произошла ошибка: ' . mysqli_error($con) . '</p>';
+      }
+    }
+
+    if (isset($_GET['del_id'])) {
+      $sql = mysqli_query($con, "DELETE FROM `content` WHERE `ID` = {$_GET['del_id']}");
+      if ($sql) {
+        echo "<p>Запись удалена.</p>";
+      } else {
+        echo '<p>Произошла ошибка: ' . mysqli_error($con) . '</p>';
+      }
+    }
+    if (isset($_GET['red_id'])) {
+      $sql = mysqli_query($con, "SELECT `text` FROM `content` WHERE `ID`={$_GET['red_id']}");
+      $product = mysqli_fetch_array($sql);
+    }
+  ?>
+  <?php 
+     echo $result['text'];
+   ?>
+  <form action="" method="post">
+      <textarea name="text" cols="100" rows="50"> <?= isset($_GET['red_id']) ? $product['text'] : ''; ?> </textarea>
+      <input type="submit" value="OK">
+      <input type="submit" value="Применить изменения">
+      <a href="statya.php">Выйти из редактора</a>
+  </form>
+    <?php
+      $sql = mysqli_query($con, 'SELECT `text` FROM `content`');
+      while ($result = mysqli_fetch_array($sql)) {
+        echo "<a href='?del_id=1'>Удалить</a> <br>" .
+             "<a href='?red_id=1'>Изменить</a>" ;
+      } }
+      else echo $result['text']; //Для обычных юзеров
+    ?>
                 </div>
          </div>
          <div id="bad_float"> <div align="center">Хамидуллин Артур</div> <div align="center">2019</div>
